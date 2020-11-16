f6f090966f0911011f0289bfb51ea17a8d99e33f
honglinhe <68503051+honglinhe@users.noreply.github.com>
2020-11-16 21:53:09 +0800

    异常处理

--- src/异常处理/ThrowTest.java
+++ src/异常处理/ThrowTest.java
@@ -2,6 +2,9 @@ package 异常处理;
 
 /**
  * throw:手动抛出异常对象
+ * throw和 throws区别:
+ * throw表示抛出一个异常类的对象,生成异常对象的过程。声明在方法体内
+ * throws属于异常处理的一种方式,声明在方法的声明处
  */
 public class ThrowTest {
     public static void main(String[] args) {
